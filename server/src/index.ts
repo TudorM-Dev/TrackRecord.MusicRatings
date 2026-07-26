68ee5a0f05720209b0fef609f72675b289de2299
Tudor Mutică <tudormutica21@gmail.com>
2026-07-26 20:21:11 +0300

    Add Express server with health endpoint

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = Number(process.env.PORT ?? 3000);
+
+app.get("/api/health", (req, res) => {
+  res.json({ status: "ok sir" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
